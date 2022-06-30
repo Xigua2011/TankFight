2a9a687fbac616d70870e93c8c72e4a58ab77a6a
Xigua2011 <67436019+Xigua2011@users.noreply.github.com>
2022-06-30 20:39:40 +0100

    explosion sound

--- explosion.gd
+++ explosion.gd
@@ -9,6 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$particles.emitting = true
+	$sound.playing = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
